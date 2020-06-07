5a8c2d041737ac21f10f9de9acf14806ed7e6282
Leilani Leach <leilanileach@yahoo.com>
2020-06-06 17:51:15 -0700

    define state for App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    allTopics: ['test1', 'test2', 'test3', 'test4'],
+    // filteredTopics: [...allTopics],
+    selectedTopic: null,
+    display: 'question',
+  };
+  getSuggestion = () => {
+    let index = Math.floor(Math.random() * this.state.allTopics.length);
+    console.log(this.state.allTopics[index]);
+    this.setState({ display: 'suggestion' });
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Would you like a suggestion for what to work on today?</p>
+          <button onClick={this.getSuggestion}>Yes</button>
+          <button onClick={() => this.setState({ display: 'nope' })}>No</button>
+        </header>
+        <div className="hidden" id="results">
+          <p>Results!</p>
+        </div>
+        {this.state.display === 'nope' && (
+          <div>
+            <p>Ok, fine. Do whatever you want.</p>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
